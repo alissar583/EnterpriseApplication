02f1dd6f3147f12ae5b817dcef126289b3eaef62
alisar.futurecode <alissarkousa@gmail.com>
2023-11-21 21:49:09 +0300

    register -- login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use PragmaRX\Google2FALaravel\Support\Authenticator;
+
+class AuthController extends Controller
+{
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(RegisterRequest $request)
+    {
+        $input = $request->validated();
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['name'] =  $user->name;
+
+        $authenticator = app(Authenticator::class)->boot($user);
+        $secretKey = $authenticator->generateSecretKey();
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+
+            return  'User login successfully.';
+        } else {
+            return 'Unauthorised.';
+        }
+    }
+}
