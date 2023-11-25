74c30a447cd3d8516bc188dd3b99887a8a5631a7
alisar.futurecode <alissarkousa@gmail.com>
2023-11-25 22:45:55 +0300

    reports

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,16 +6,22 @@ use App\Enums\FileStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class File extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['group_id', 'path'];
+    protected $fillable = ['group_id', 'path', 'status'];
 
-    protected $casts  = [FileStatusEnum::class];
+    protected $casts  = ['status' => FileStatusEnum::class];
     public function group(): BelongsTo
     {
         return $this->belongsTo(Group::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_file');
     }
 }
